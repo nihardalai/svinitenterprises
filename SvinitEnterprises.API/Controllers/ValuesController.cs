29210e7bffd2a96a291c00904c7cbc139d5d3a1b
Nihar Ranjan Dalai <nihar.dalai@gmail.com>
2019-07-28 16:50:38 -0500

    Authentication and authorization continued.

--- SvinitEnterprises.API/Controllers/ValuesController.cs
+++ SvinitEnterprises.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace SvinitEnterprises.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
