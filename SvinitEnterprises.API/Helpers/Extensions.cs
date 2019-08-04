8062612c02ca324a30a7c5087c23fb8ae54edaf7
Nihar Ranjan Dalai <nihar.dalai@gmail.com>
2019-08-04 15:39:36 -0500

    Error handling

---
+++ SvinitEnterprises.API/Helpers/Extensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Http;
+
+namespace SvinitEnterprises.API.Helpers
+{
+    public static class Extensions
+    {
+        public static void AddApplicationError(this HttpResponse httpResponse, string message)
+        {
+            httpResponse.Headers.Add("Application-Error", message);
+            httpResponse.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            httpResponse.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
